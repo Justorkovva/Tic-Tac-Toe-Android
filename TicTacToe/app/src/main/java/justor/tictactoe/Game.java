25db8657fd846122381945032d7f98395664a4ec
Justorkovva <j.mackos@wp.pl>
2017-09-13 17:27:54 +0200

    playable
    playable, but only once ; p

--- TicTacToe/app/src/main/java/justor/tictactoe/Game.java
+++ TicTacToe/app/src/main/java/justor/tictactoe/Game.java
@@ -2,12 +2,33 @@ package justor.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.color.black;
 
 public class Game extends AppCompatActivity {
+    private boolean x = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public void button_click(View v) {
+        Button button = (Button) v;
+        if(button.getText().equals("."))
+        {
+            if (x) {
+                ((Button) v).setText("X");
+                button.setTextColor(getResources().getColor(black));
+                x = false;
+            } else {
+                ((Button) v).setText("O");
+                button.setTextColor(getResources().getColor(black));
+                x = true;
+            }
+        }
     }
 }
